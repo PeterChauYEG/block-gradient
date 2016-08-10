8112d3a2c96726ac55c08a65a06625b3226e31a0
Peter C <peter.chau.esq@gmail.com>
2016-08-10 21:23:27 +0000

    implemented linear-gradient

--- src/imports/components/Main.js
+++ src/imports/components/Main.js
@@ -11,8 +11,22 @@ import BlocksContainer from './BlocksContainer'
 
 class Main extends Component {
   render() {
+    const { blocks } = this.props
+    
+    // calculate gradient
+    const gradient = blocks.reduce((prev, current, i, array) => {
+      if (i < array.length - 1) {
+        return prev + current.color + ', '
+      }
+      return prev + current.color + ')'
+    }, 'linear-gradient(')
+    
+    const style = {
+      background: gradient
+    }
+    
     return (
-      <div className="Main">
+      <div className="Main" style={style}>
         <BlocksContainer {...this.props} />
       </div>
     )
